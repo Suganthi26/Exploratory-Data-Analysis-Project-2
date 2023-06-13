e5feb9e2229e8d8ea6c84f5df71e516680cae214
Suganthi Jawahar <101238607+Suganthi26@users.noreply.github.com>
2023-06-13 19:31:18 -0400

    Add files via upload

---
+++ Baltimore vs LA.R
@@ -0,0 +1,12 @@
+balti_la_year <- motor_emi %>% filter(fips == "24510" | fips == "06037") %>% 
+  group_by(fips, year) %>% summarise(total = sum(Emissions))
+balti_la_year <- mutate(balti_la_year, 
+                        Unit = ifelse(fips == "24510", "Baltimore City", 
+                                      ifelse(fips == "06037", "Los Angeles County")))
+ggplot(balti_la_year, aes(factor(year), total, 
+                          fill = Unit, label = round(total))) + 
+  geom_bar(stat = "identity") + facet_grid(. ~ Unit) + 
+  ggtitle("Total Motor Vehicle Emissions") +
+  xlab("Year") + ylab("Pm2.5 Emissions in Tons") +
+  theme(plot.title = element_text(hjust = 0.5)) + ylim(c(0, 8000)) +
+  theme_classic() + geom_text(size = 4, vjust = -1)
